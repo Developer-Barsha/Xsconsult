2fa7a6f68560d8bfe6cab5457808fea643e815ce
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2023-01-16 01:58:29 +0600

    first night codes done

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,10 +1,65 @@
 import React from 'react';
 import './Home.css'
+import banner from './../../../images/banner.png'
+import curriculam from './../../../images/curriculam.png'
+import projectBased from './../../../images/project-based.png'
+import brand1 from './../../../images/brand1.png'
+import brand2 from './../../../images/brand2.png'
+import brand3 from './../../../images/brand3.png'
+import brand4 from './../../../images/brand4.png'
+import brand5 from './../../../images/brand5.png'
 
 const Home = () => {
+
     return (
         <div>
+            <div className="banner grid-sec grid lg:grid-cols-2 grid-cols-1 items-start px-16 py-20">
+                <div className='flex flex-col gap-10 pt-10'>
+                    <h1 className="text-6xl">Take your <span>cloud DevOps</span> career to the next <span>level</span></h1>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                    <button className='common-btn'>Book a free demo</button>
+                </div>
+                <div className='grid-img lg:px-0 px-4 flex justify-end'>
+                    <img src={banner} alt="" />
+                    <div className='img-bg'></div>
+                </div>
+            </div>
+
+            <div className="brands grid grid-cols-1 lg:grid-cols-5 md:grid-cols-2 gap-5 items-center justify-center px-16 py-3 bg-[#D9D9D9]">
+                <img src={brand1} alt="" />
+                <img src={brand2} alt="" />
+                <img src={brand3} alt="" />
+                <img src={brand4} alt="" />
+                <img src={brand5} alt="" />
+            </div>
+
+            <div className="curriculam grid-sec grid lg:grid-cols-2 grid-cols-1 items-center px-16 py-20">
+                <div className='grid-img flex justify-start'>
+                    <img src={curriculam} alt="" />
+                    <div className='img-bg'></div>
+                </div>
+                <div className='flex flex-col items-end text-right gap-10 pt-10'>
+                    <h1 className="text-5xl">Essential knowledge for <span>beginning a career in DevOps</span></h1>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                    <button className='btn rounded-full border-lime-500 hover:bg-lime-500 py-3 px-5 border flex items-center gap-3'>See Curriculam <i class="fa-solid fa-arrow-right"></i></button>
+                </div>
+            </div>
+
+            <div className='guide py-5 px-12'>
+                <h1 className='text-5xl text-center'>Guide to a career as a <br/> <span>DevOps engineer</span></h1>
+            </div>
             
+            <div className="project-based grid-sec grid lg:grid-cols-2 grid-cols-1 items-start px-16 py-20">
+                <div className='flex flex-col gap-10 pt-10'>
+                    <h1 className="text-6xl">Project-Based Learning: Obtaining Real-World Experience</h1>
+                    <p>With us, you will learn High-Value DevOps skills from industry professionals that are usually obtainable while on a paid job.</p>
+                    <button className='btn text-[#71B131] underline hover:no-underline flex items-center gap-3'>Download Curriculam <i class="fa-solid fa-arrow-right"></i></button>
+                </div>
+                <div className='grid-img lg:px-0 px-4 flex justify-end'>
+                    <img src={projectBased} alt="" />
+                    <div className='img-bg'></div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,25 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import logo from './../../../images/logo.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
-        <header className='flex justify-between items-center py-5 px-10 text-white'>
-            <img src={logo} alt="" />
-            <ul className='gap-10'>
-                <li>About Us</li>
-                <li>Contact Us</li>
-                <li>FAQ</li>
-                <li>Pricing</li>
-                <li>Curriculam</li>
-            </ul>
-            <div className="button">
-                <button class="gradient-border-button">
-                    Click me
+        <section>
+            <header className='lg:flex hidden justify-between items-center py-5 px-10'>
+                <img src={logo} alt="" />
+
+                <ul className='gap-10'>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>FAQ</li>
+                    <li>Pricing</li>
+                    <li>Curriculam</li>
+                </ul>
+                <div className="button">
+                    <button>
+                        Eroll Now
+                    </button>
+                </div>
+            </header>
+
+            <header className='lg:hidden flex'>
+                {/* menu hide and show icon */}
+                <button onClick={() => setShowMenu(!showMenu)} className='lg:hidden text-2xl absolute' style={{zIndex:1000}}>
+                    {showMenu ?
+                        <i class="fa-solid fa-xmark fixed top-10 left-10"></i>
+                        :
+                        <i class="fa-solid fa-bars fixed top-10 right-10"></i>
+                    }
                 </button>
-            </div>
-            {/* <button className='btn rounded-full border-green-500'>Enroll Now</button> */}
-        </header>
+
+                {/* header nav */}
+                {showMenu &&
+                    <div className='lg:w-0 transition-all flex flex-col items-center text-center justify-center gap-10 bg-black w-full h-screen fixed' style={{zIndex:100}}>
+                        <ul className='flex flex-col gap-10'>
+                            <li>About Us</li>
+                            <li>Contact Us</li>
+                            <li>FAQ</li>
+                            <li>Pricing</li>
+                            <li>Curriculam</li>
+                        </ul>
+                        <div className="button">
+                            <button>
+                                Eroll Now
+                            </button>
+                        </div>
+                    </div>
+                }
+            </header>
+        </section>
     );
 };
 
